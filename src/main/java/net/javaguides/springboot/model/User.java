9d0f1570afe2617703500965d22fe2cd06bab63f
Fabricio Belam <36649120+fbelam@users.noreply.github.com>
2024-09-04 21:59:01 -0300

    Primeiro teste de Back

---
+++ src/main/java/net/javaguides/springboot/model/User.java
@@ -0,0 +1,63 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstname;
+	
+	@Column(name = "last_name")
+	private String lastname;
+	
+	private String email;
+	
+	public User() {
+		
+	}
+	
+	
+	public User(String firstname, String lastname, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
